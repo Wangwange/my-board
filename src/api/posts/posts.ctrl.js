6f22bf89c789fa52af0494fe213547748ff1c0cf
Wangwange <ltk6031@naver.com>
2020-04-05 16:06:17 +0900

    주석 작성

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -1,6 +1,7 @@
 const { Types } = require("mongoose");
 const Post = require("../../models/post");
 
+// 게시물 ID가 필요한 요청에 한해 ID 검증
 exports.checkObjectId = (ctx, next) => {
   const { id } = ctx.params;
   if (!Types.ObjectId.isValid(id)) {
@@ -10,6 +11,7 @@ exports.checkObjectId = (ctx, next) => {
   return next();
 };
 
+// 포스트 게시 - (title, body, tags) => post
 exports.write = async (ctx) => {
   const { title, body, tags } = ctx.request.body;
   try {
@@ -23,7 +25,7 @@ exports.write = async (ctx) => {
   } catch (e) {
     console.log(e);
     if (e.name === "ValidationError") {
-      ctx.status = 400;
+      ctx.status = 400; //필수 항목이 빠진 요청이면 Bad Request
       return;
     }
     ctx.status = 500;
@@ -31,6 +33,7 @@ exports.write = async (ctx) => {
   }
 };
 
+// 개별 포스트 조회 - id => post
 exports.read = async (ctx) => {
   const { id } = ctx.params;
   try {
@@ -46,6 +49,7 @@ exports.read = async (ctx) => {
   }
 };
 
+// 포스트 목록 조회 - () => posts
 exports.list = async (ctx) => {
   try {
     const posts = await Post.find().exec();
